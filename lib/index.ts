d703153006e7cebc3161b225eb02a51adab3c25a
Thomas Meadows <thomas@itpro.tv>
2017-05-06 14:55:29 -0400

    upgraded server to be exportable in a typescript module

--- lib/index.ts
+++ lib/index.ts
@@ -1,6 +1,3 @@
-import { Log } from '../core/src/log';
-const body = require('koa-json-body');
-
 export function DKoa(): any {
   return function(OriginalClassConstructor: any) {
 
@@ -8,30 +5,26 @@ export function DKoa(): any {
       const origin = new OriginalClassConstructor();
       origin.__seatbelt__ = 'server';
       origin.__seatbelt_strap__ = function(classesByType: any) {
-        this.log = new Log('Koa');
         this.Koa = require('koa');
         this.app = new this.Koa();
+        this.body = require('koa-json-body');
         this.port = process.env.port || 3000;
-        this.log = new Log('Express');
         this.router = require('koa-router')();
-        this.app.use(body({ limit: '10kb', fallback: true }));
+        this.app.use(this.body({ limit: '10kb', fallback: true }));
         origin.__controller_wrapper__ = function (controllerFunction: Function, ctx: any, next: Function) {
-          console.log(JSON.stringify(ctx.request));
-          controllerFunction(Object.assign(ctx,
-            {
-              next,
-              reply: (response: any) => {
-                ctx.body = response;
-              },
-              params: Object.assign(
-                {},
-                typeof ctx.req.params === 'object' ? ctx.req.params : {},
-                typeof ctx.request.body === 'object' ? ctx.request.body : {}
-                ,
-                typeof ctx.request.query === 'object' ? ctx.request.query : {}
-              )
-            }
-          ));
+          controllerFunction({
+            next,
+            send: (response: any) => {
+              ctx.body = response;
+            },
+            params: Object.assign(
+              {},
+              typeof ctx.req.params === 'object' ? ctx.req.params : {},
+              typeof ctx.request.body === 'object' ? ctx.request.body : {}
+              ,
+              typeof ctx.request.query === 'object' ? ctx.request.query : {}
+            )
+          }, ctx);
         };
         if (classesByType['route']) {
           classesByType['route'].forEach((route: any) => {
