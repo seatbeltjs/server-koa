54140ed747d275c17dbe4eb9093b3893231a0357
Thomas Meadows <thomas@itpro.tv>
2017-05-21 18:19:02 -0400

    modified entry point and dependencies

--- lib/index.ts
+++ lib/index.ts
@@ -1,56 +1,46 @@
+import * as Koa from 'koa';
+import * as Router from 'koa-router';
+import * as bodyParser from 'koa-bodyparser';
+
 export function DKoa(): any {
-  return function(OriginalClassConstructor: any) {
-
-    return function () {
-      const origin = new OriginalClassConstructor();
-      origin.__seatbelt__ = 'server';
-      origin.__seatbelt_strap__ = function(classesByType: any) {
-        this.Koa = require('koa');
-        this.app = new this.Koa();
-        this.body = require('koa-json-body');
-        this.port = process.env.port || 3000;
-        this.router = require('koa-router')();
-        this.app.use(this.body({ limit: '10kb', fallback: true }));
-        origin.__controller_wrapper__ = function (controllerFunction: Function, ctx: any, next: Function) {
-          controllerFunction({
-            next,
-            send: (response: any) => {
-              ctx.body = response;
-            },
-            params: Object.assign(
-              {},
-              typeof ctx.req.params === 'object' ? ctx.req.params : {},
-              typeof ctx.request.body === 'object' ? ctx.request.body : {}
-              ,
-              typeof ctx.request.query === 'object' ? ctx.request.query : {}
-            )
-          }, ctx);
-        };
-        if (classesByType['route']) {
-          classesByType['route'].forEach((route: any) => {
-            const policies: any[] = [];
-            route.__seatbelt_config__.policies.forEach((routePolicyName: string) => {
-              classesByType['policy'].forEach((policy: any) => {
-                if (routePolicyName === policy.__name__) {
-                  policies.push(policy);
-                }
-              });
-            });
-
-            route['__seatbelt_config__'].type.forEach((eachType: string) => {
-              route['__seatbelt_config__'].path.forEach((eachPath: string) => {
-                policies.forEach(policy => {
-                  this.router[eachType.toLowerCase()](eachPath, (ctx: any, next: Function) => origin.__controller_wrapper__(policy.controller, ctx, next));
+  return function(originalClassConstructor: new () => {}) {
+    return class extends originalClassConstructor {
+      public __seatbelt__: string;
+      public __seatbelt_strap__: Function;
+      constructor () {
+        super();
+        this.__seatbelt__ = 'server';
+        this.__seatbelt_strap__ = function(routes: any[]) {
+          this.server = new Koa();
+          this.port = process.env.port || 3000;
+          this.router = new Router();
+          this.server.use(bodyParser({ enableTypes: ['json'] }));
+          this.__controller_wrapper__ = function (route: any, ctx: any, next: Function) {
+            route.controller({
+              send: (response: any) => {
+                ctx.body = response;
+              },
+              params: Object.assign(
+                {},
+                typeof ctx.req.params === 'object' ? ctx.req.params : {},
+                typeof ctx.request.body === 'object' ? ctx.request.body : {},
+                typeof ctx.request.query === 'object' ? ctx.request.query : {}
+              )
+            }, ctx);
+          };
+          if (routes && Array.isArray(routes)) {
+            routes.forEach((route: any) => {
+              route['__seatbelt_config__'].type.forEach((eachType: string) => {
+                route['__seatbelt_config__'].path.forEach((eachPath: string) => {
+                  this.router[eachType.toLowerCase()](eachPath, (ctx: any, next: Function) => this.__controller_wrapper__(route, ctx, next));
                 });
-                this.router[eachType.toLowerCase()](eachPath, (ctx: any, next: Function) => origin.__controller_wrapper__(route.controller, ctx, next));
               });
             });
-          });
-        }
-        this.app.use(this.router.routes());
-        this.app.listen(this.port);
+          }
+          this.server.use(this.router.routes());
+          this.server.listen(this.port);
+        };
       };
-      return origin;
     };
   };
 }
