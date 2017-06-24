459828221e73f14e523728f2de22126102f32873
Thomas Meadows <thomasmeadows@gmail.com>
2017-06-23 22:22:04 -0400

    modified the way servers are created

--- lib/index.d.ts
+++ lib/index.d.ts
@@ -1,1 +1,16 @@
-export declare function DKoa(): any;
+/// <reference types="koa" />
+/// <reference types="koa-router" />
+/// <reference types="koa-bodyparser" />
+import * as Koa from 'koa';
+import * as Router from 'koa-router';
+import { Log } from '@seatbelt/core';
+export declare class KoaServer {
+    log: Log;
+    server: Koa;
+    port: number;
+    router: Router;
+    conformServerControllerToSeatbeltController: Function;
+    config: Function;
+    init: Function;
+}
+export declare const server: KoaServer;
--- lib/index.ts
+++ lib/index.ts
@@ -1,46 +1,42 @@
 import * as Koa from 'koa';
 import * as Router from 'koa-router';
 import * as bodyParser from 'koa-bodyparser';
+import { DServerRegister, IServerRequest, IServerResponse, IServerRoute, Log } from '@seatbelt/core';
 
-export function DKoa(): any {
-  return function(originalClassConstructor: new () => {}) {
-    return class extends originalClassConstructor {
-      public __seatbelt__: string;
-      public __seatbelt_strap__: Function;
-      constructor () {
-        super();
-        this.__seatbelt__ = 'server';
-        this.__seatbelt_strap__ = function(routes: any[]) {
-          this.server = new Koa();
-          this.port = process.env.port || 3000;
-          this.router = new Router();
-          this.server.use(bodyParser({ enableTypes: ['json'] }));
-          this.__controller_wrapper__ = function (route: any, ctx: any, next: Function) {
-            route.controller({
-              send: (response: any) => {
-                ctx.body = response;
-              },
-              params: Object.assign(
-                {},
-                typeof ctx.req.params === 'object' ? ctx.req.params : {},
-                typeof ctx.request.body === 'object' ? ctx.request.body : {},
-                typeof ctx.request.query === 'object' ? ctx.request.query : {}
-              )
-            }, ctx);
-          };
-          if (routes && Array.isArray(routes)) {
-            routes.forEach((route: any) => {
-              route['__seatbelt_config__'].type.forEach((eachType: string) => {
-                route['__seatbelt_config__'].path.forEach((eachPath: string) => {
-                  this.router[eachType.toLowerCase()](eachPath, (ctx: any, next: Function) => this.__controller_wrapper__(route, ctx, next));
-                });
-              });
-            });
-          }
-          this.server.use(this.router.routes());
-          this.server.listen(this.port);
-        };
-      };
-    };
+@DRegisterServer()
+export class KoaServer {
+  public log: Log = new Log('KoaServer');
+  public server: Koa = new Koa();
+  public port: number = process.env.port || 3000;
+  public router: Router = new Router();
+  public conformServerControllerToSeatbeltController: Function = function (route: any, ctx: Koa.Context) {
+    route.controller({
+      send: (response: any) => {
+        ctx.body = response;
+      },
+      params: Object.assign(
+        {},
+        typeof ctx.request.body === 'object' ? ctx.request.body : {},
+        typeof ctx.request.query === 'object' ? ctx.request.query : {}
+      )
+    }, ctx);
+  };
+  public config: Function = function(routes: any[]) {
+    this.server.use(bodyParser({ enableTypes: ['json'] }));
+    if (routes && Array.isArray(routes)) {
+      routes.forEach((route: any) => {
+        route['__seatbelt_config__'].type.forEach((eachType: string) => {
+          route['__seatbelt_config__'].path.forEach((eachPath: string) => {
+            this.router[eachType.toLowerCase()](eachPath, (ctx: Koa.Context) => this.conformServerControllerToSeatbeltController(route, ctx));
+          });
+        });
+      });
+    }
+  };
+  public init: Function  = function(routes: any[]) {
+    this.server.use(this.router.routes());
+    this.server.listen(this.port);
   };
 }
+
+export const server: KoaServer = new KoaServer();
